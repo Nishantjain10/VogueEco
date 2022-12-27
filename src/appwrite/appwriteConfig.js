2cacf9671dfefeef46d019a1f491c6e40970b59d
Nishantjain10 <nishant.jain.775@gmail.com>
2022-12-27 19:21:31 +0530

    digital ocean config production deployment

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -2,12 +2,11 @@ import {Client, Account, Databases} from 'appwrite'
 const client = new Client();
 
 client
-    .setEndpoint('http://157.245.101.77/v1')
-    .setProject('63aa9d800c7370ab6efb');
-
+    .setEndpoint('http://143.244.134.240/v1')
+    .setProject('63aaeaf74d0ae48e48a5');
 
 export const account = new Account(client)
 
 //Database
 
-export const databases = new Databases(client, "63aa9d800c7370ab6efb")
+export const databases = new Databases(client, "63aaef5036cc3edd20a3")
