f1351a603400361cfc9f60f346fd995bfb4e3e4b
Nishant Jain <89480234+Nishantjain10@users.noreply.github.com>
2023-04-12 22:15:17 +0530

    Update appwriteConfig.js

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -4,7 +4,7 @@ const client = new Client();
 const appwrite_api_key = process.env.REACT_APP_APPWRITE_API_KEY;
 
 client
-  .setEndpoint("http://appwrite.vogueeco.us/v1")
+  .setEndpoint("https://appwrite.vogueeco.us/v1")
   .setProject(appwrite_api_key);
 
 export const account = new Account(client);
